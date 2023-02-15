c70572d1d30bc2821dea76dd99c9b7bec2db25a7
stonega <xijieyin@gmail.com>
2023-02-15 21:48:33 +0800

    fix: update api

--- pages/api/analyze.ts
+++ pages/api/analyze.ts
@@ -12,13 +12,14 @@ export default async function handler(
   res: NextApiResponse,
 ) {
   if (req.method === "POST") {
-    const { data } = req.body;
+    const { data } = JSON.parse(req.body);
+    const input = data.replace("\n", " ");
     const response = await openai.createEmbedding({
-      input: 'Get book names from the text\n' + data,
+      input: "Get book names from the text: " + input,
       model: "text-embedding-ada-002",
     });
     res.status(200).json({ data: JSON.stringify(response) });
   } else {
-    // Handle any other HTTP method
+    res.status(500).json({ data: "error" });
   }
 }
