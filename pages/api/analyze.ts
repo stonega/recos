81982b2f919d9dea1f8d2130b1584f5363f31017
stonega <xijieyin@gmail.com>
2023-02-15 21:31:35 +0800

    fix: api

--- pages/api/analyze.ts
+++ pages/api/analyze.ts
@@ -1,22 +1,24 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
 import { Configuration, OpenAIApi } from "openai";
 
 const configuration = new Configuration({
-    organization: "org-0X0HpVK7VBKcdfxshYHYcNNA",
-    apiKey: process.env.OPENAI_API_KEY,
+  organization: "org-0X0HpVK7VBKcdfxshYHYcNNA",
+  apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    if (req.method === 'POST') {
-      // Process a POST request
-      const {data} = req.body
-      const response = await openai.createEmbedding({
-        'input': data,
-        "model": "text-embedding-ada-002",
-      })
-      res.status(200).json({ data: response })
-    } else {
-      // Handle any other HTTP method
-    }
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  if (req.method === "POST") {
+    const { data } = req.body;
+    const response = await openai.createEmbedding({
+      input: 'Get book names from the text\n' + data,
+      model: "text-embedding-ada-002",
+    });
+    res.status(200).json({ data: JSON.stringify(response) });
+  } else {
+    // Handle any other HTTP method
   }
+}
