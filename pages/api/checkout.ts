f16871ef39edcc59ebd0f9b888a00dc71d2769f6
stonega <xijieyin@gmail.com>
2023-05-06 00:41:08 +0800

    fix: checkout api

--- pages/api/checkout.ts
+++ pages/api/checkout.ts
@@ -69,7 +69,7 @@ export default async function handler(
         },
       );
       const newCheckout = await response.json()
-      res.status(200).json({ link: newCheckout.data.attributes.url});
+      res.status(200).json({ link: newCheckout.attributes.url});
     } catch (err) {
       console.error(err);
       res.status(500).json({ error: err });
