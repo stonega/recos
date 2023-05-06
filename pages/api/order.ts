02d7dd75fab931a57624bf9b66100e6a9b4784dc
stonega <xijieyin@gmail.com>
2023-05-06 23:02:12 +0800

    fix: order api

--- pages/api/order.ts
+++ pages/api/order.ts
@@ -1,11 +1,31 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import crypto from "crypto";
+import bodyParser from 'body-parser'
+
+export function runMiddleware(req: NextApiRequest, res: NextApiResponse, fn: Function) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result: Object) => {
+      if (result instanceof Error) {
+        return reject(result)
+      }
+
+      return resolve(result)
+    })
+  })
+}
+export const config = {
+  api: {
+    bodyParser: false,
+  }
+}
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
   if (req.method === "POST") {
+    console.log(req.body);
+    await runMiddleware(req, res, bodyParser.raw({ type: 'application/json' }))
     const secret = process.env.LEMON_SECRET;
     const hmac = crypto.createHmac("sha256", secret!);
     const digest = Buffer.from(hmac.update(req.body).digest("hex"), "utf8");
@@ -16,7 +36,7 @@ export default async function handler(
     if (!crypto.timingSafeEqual(digest, signature)) {
       res.status(500).json({ error: "Invalid signature" });
     }
-    console.log(req.body);
+    await runMiddleware(req, res, bodyParser.json())
     const userId = req.body["meta"]["custom_data"]["user_id"];
     const credit = 10;
     const user = await prisma?.user.findUnique({
