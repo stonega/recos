8db7f8b046eec977bd5979f55b149fcacd4e0db0
stonega <xijieyin@gmail.com>
2023-05-14 01:45:35 +0800

    fix: update confetti

--- components/shared/confetti.tsx
+++ components/shared/confetti.tsx
@@ -7,13 +7,29 @@ const Confetti = () => {
     windowSize: { width, height },
   } = useWindowSize();
   const [show, setShow] = React.useState(true);
+  const [opacity, setOpacity] = React.useState(1);
   useEffect(() => {
     const timer = setTimeout(() => {
       setShow(false);
-    }, 10000);
-    return () => clearTimeout(timer);
+    }, 3000);
+    let interval: number | NodeJS.Timer;
+    const opacityTimer = setTimeout(() => {
+      interval = setInterval(() => setOpacity((prev) => prev - 0.2), 100);
+    }, 2500);
+    return () => {
+      clearTimeout(timer);
+      clearTimeout(opacityTimer);
+      if (interval) clearInterval(interval);
+    };
   });
-  if (show) return <ReactConfetti width={width ?? 0} height={height ?? 0} />;
+  if (show)
+    return (
+      <ReactConfetti
+        width={width ?? 0}
+        height={height ?? 0}
+        opacity={opacity}
+      />
+    );
   return <></>;
 };
 
