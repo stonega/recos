b86a1141a8429f3ea5ee1039dff6c42f8a52924e
stonega <xijieyin@gmail.com>
2023-07-15 19:18:48 +0800

    style: update email template

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -83,27 +83,27 @@ function html({ url, host, email }: Record<"url" | "host" | "email", string>) {
 <body>
   <table width="100%" border="0" cellspacing="20" cellpadding="0" style="border-radius: 10px; background: ${mainBackgroundColor}; max-width: 600px; margin: auto; border-radius: 10px;">
     <tr>
-      <td align="left" style="margin: 10px; font-size: 24px; background: ${mainBackgroundColor}; color: ${textColor};">
+      <td align="left" style="margin: 10px; font-size: 24px; background: ${mainBackgroundColor}; color: ${textColor}; font-family: serif">
         <strong>Recos.</strong>
       </td>
       <td align=="right"><img src="https://recos.vercel.app/logo.png" width="40" style="float: right; margin: 10px"></td>
     </tr>
     <tr>
-      <td align="center" style="padding: 10px 0px 0px 0px; font-size: 18px; font-family: Helvetica, Arial, sans-serif; color: ${textColor};">
+      <td colspan="5" align="center" style="padding: 10px 0px 0px 0px; font-size: 18px; font-family: Helvetica, Arial, sans-serif; color: ${textColor};">
         Sign in as <strong>${escapedEmail}</strong>
       </td>
     </tr>
     <tr>
-      <td align="center" style="padding: 20px 0;">
+      <td colspan="5" align="center" style="padding: 20px 0;">
         <table border="0" cellspacing="0" cellpadding="0">
           <tr>
-            <td align="center" style="border-radius: 5px; padding: 4px; background: ${buttonBackgroundColor}"><a href="${url}" target="_blank" style="font-size: 18px; font-family: Helvetica, Arial, sans-serif; color: ${buttonTextColor}; text-decoration: none; border-radius: 5px; padding: 10px 20px; border: 1px solid ${buttonBorderColor}; display: inline-block; font-weight: bold;">Sign in</a></td>
+            <td align="center" style="border-radius: 5px; padding: 4px; background: ${buttonBackgroundColor}"><a href="${url}" target="_blank" style="font-size: 18px; font-family: Helvetica, Arial, sans-serif; color: ${buttonTextColor}; text-decoration: none; border-radius: 5px; padding: 10px 20px; display: inline-block; font-weight: bold;">Sign in</a></td>
           </tr>
         </table>
       </td>
     </tr>
     <tr>
-      <td align="center" style="padding: 0px 0px 10px 0px; font-size: 14px; line-height: 22px; font-family: Helvetica, Arial, sans-serif; color: ${textColor};">
+      <td colspan="5" align="center" style="padding: 0px 0px 10px 0px; font-size: 14px; line-height: 22px; font-family: Helvetica, Arial, sans-serif; color: ${textColor};">
         If you did not request this email you can safely ignore it.
       </td>
     </tr>
